78ceaa0c8298e4a031490a0f50a78bd32a38fd89
null1998 <1050510316@qq.com>
2020-10-30 21:22:17 +0800

    从ArrayList向上到Iterable的开头文档看完

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+N
+src/com/hyd/studyjdk/Main.java,7\1\7163cd6929d9d409aaef6af9c800645dc08f6e6e
---
+++ src/com/hyd/studyjdk/Main.java
@@ -0,0 +1,15 @@
+package com.hyd.studyjdk;
+
+import java.util.ArrayList;
+
+/**
+ * @author yanduohuang
+ * @version 1.0
+ * @date 2020/10/30 16:51
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(1);
+    }
+}
